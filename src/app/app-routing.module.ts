b687ee2645c717830e1902f08782556d9e3f3036
Navin3d <smnavin65@mail.com>
2023-01-15 21:40:28 +0530

    changed router and navLinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,5 +18,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-export const RoutingComponents = [HomePageComponent, EyeglassPageComponent, ContactPageComponent, SignupPageComponent];
